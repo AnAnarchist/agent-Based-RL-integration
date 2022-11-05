f6a6b32472ee4c672303d3f4b2e9b793d19ef97d
AnAnarchist <130145319+AnAnarchist@users.noreply.github.com>
2022-11-05 14:38:43 +0000

    fix comma

--- src/agt/rl/algorithm/AlgorithmParameter.java
+++ src/agt/rl/algorithm/AlgorithmParameter.java
@@ -1,4 +1,11 @@
 package rl.algorithm;
 
 import java.util.Map;
-import ja
+import java.util.Map.Entry;
+
+import jason.asSyntax.Term;
+
+public class AlgorithmParameter {
+	
+	private static final String ALPHA_TERM = "alpha";
+	privat
