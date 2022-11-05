8ec48c1c758e1ca3bb0815e6c341dbe8940b8fa4
AnAnarchist <130145319+AnAnarchist@users.noreply.github.com>
2022-11-05 17:43:56 +0000

    print statements

--- src/agt/rl/algorithm/AlgorithmParameter.java
+++ src/agt/rl/algorithm/AlgorithmParameter.java
@@ -8,4 +8,6 @@ import jason.asSyntax.Term;
 public class AlgorithmParameter {
 	
 	private static final String ALPHA_TERM = "alpha";
-	privat
+	private static final String GAMMA_TERM = "gamma";
+	private static final String EPSILON_TERM = "epsilon";
+	private stati
