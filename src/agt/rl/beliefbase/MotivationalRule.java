8e1b9e74de6106c8de2e125e93e38dcc9ae34d88
AnAnarchist <130145319+AnAnarchist@users.noreply.github.com>
2023-06-17 17:28:47 +0000

    Update flask_main.py

--- src/agt/rl/beliefbase/MotivationalRule.java
+++ src/agt/rl/beliefbase/MotivationalRule.java
@@ -6,4 +6,7 @@ import jason.NoValueException;
 import jason.asSemantics.Agent;
 import jason.asSemantics.Unifier;
 import jason.asSyntax.ASSyntax;
-import jaso
+import jason.asSyntax.Literal;
+import jason.asSyntax.NumberTerm;
+import jason.asSyntax.PredicateIndicator;
+import jason.asSyntax.Rule
