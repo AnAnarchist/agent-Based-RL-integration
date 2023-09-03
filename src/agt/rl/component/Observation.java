c62c0d19e61793d8e1b60df8e6c6e68f2a056ddc
AnAnarchist <130145319+AnAnarchist@users.noreply.github.com>
2023-09-03 14:31:12 +0000

    Final Fixes

--- src/agt/rl/component/Observation.java
+++ src/agt/rl/component/Observation.java
@@ -2,4 +2,11 @@ package rl.component;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.u
+import java.util.List;
+
+import jason.NoValueException;
+import jason.asSyntax.Literal;
+import jason.asSyntax.NumberTerm;
+import jason.asSyntax.Term;
+
+public cl
