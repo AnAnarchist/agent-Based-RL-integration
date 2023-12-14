55c354dc7fc26a665fb535e65e03e3fa2aef317a
AnAnarchist <130145319+AnAnarchist@users.noreply.github.com>
2023-12-14 05:32:21 +0000

    fixed typos.

--- src/env/gym/Cartpole.java
+++ src/env/gym/Cartpole.java
@@ -4,4 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 import cartago.Artifact;
-import cartago.OPERAT
+import cartago.OPERATION;
+import cartago.ObsProperty;
+import gym.rest.GymRest;
+import gym.rest.StateRest;
+import simulation.EpisodicLogger;
+
+import jason.asSynta
