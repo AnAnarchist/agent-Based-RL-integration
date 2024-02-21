188b03622b8ba6e405f80f143fa6bdff37f25896
AnAnarchist <130145319+AnAnarchist@users.noreply.github.com>
2024-02-21 08:16:58 +0000

    Update Travis CI

--- src/env/gym/Cartpole.java
+++ src/env/gym/Cartpole.java
@@ -93,4 +93,7 @@ public class Cartpole extends Artifact{
 		ObsProperty pole_position = getObsProperty("pole_position");
 		pole_position.updateValue(0, state.getState().get(2));
 		ObsProperty pole_velocity = getObsProperty("pole_velocity");
-		
+		pole_velocity.updateValue(0, state.getState().get(3));
+		if (state.isTerminal()) {
+			if (!hasObsProperty("gameover"))
+				defineObsPro
