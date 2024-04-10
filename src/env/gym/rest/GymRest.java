b4f916afceb1479cd5a479aab3e573b1e60270f1
AnAnarchist <130145319+AnAnarchist@users.noreply.github.com>
2024-04-10 18:51:52 +0000

    added missing

--- src/env/gym/rest/GymRest.java
+++ src/env/gym/rest/GymRest.java
@@ -1,3 +1,10 @@
 package gym.rest;
 
-import java.util.Map
+import java.util.Map;
+
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@SuppressWarnings("unchecked")
+pub
