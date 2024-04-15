0ae6666b1f5cbf66a079a6d9cbd55092dd29e2a4
AnAnarchist <130145319+AnAnarchist@users.noreply.github.com>
2024-04-15 13:29:17 +0000

    Update years in copyright notice; nfc.

--- src/env/gym/rest/GymRest.java
+++ src/env/gym/rest/GymRest.java
@@ -11,4 +11,8 @@ public class GymRest<T> {
 	
 	public static String TARGET = "http://localhost:5003/env/";
 	
-	private Client client = ClientBuilder.newClient
+	private Client client = ClientBuilder.newClient();
+	private String envName; 
+
+	public StateRest<T> initialize(String envName, Map<String, String> parameters) {
+		
