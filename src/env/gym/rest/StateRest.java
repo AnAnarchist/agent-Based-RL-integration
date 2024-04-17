7a6e64ad53fd5b66f8b3455776af168973ec6369
AnAnarchist <130145319+AnAnarchist@users.noreply.github.com>
2024-04-17 12:49:15 +0000

    chore: add license

--- src/env/gym/rest/StateRest.java
+++ src/env/gym/rest/StateRest.java
@@ -5,4 +5,11 @@ import java.util.List;
 public class StateRest<T> {
     private List<T> state;
     private double reward;
-    private boolea
+    private boolean terminal;
+
+    public List<T> getState() {
+        return state;
+    }
+
+    public void setState(List<T> state) {
+        
