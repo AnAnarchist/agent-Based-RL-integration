68af6db1db7ffb8f1f0cb8f485bf910527e4b1d6
AnAnarchist <130145319+AnAnarchist@users.noreply.github.com>
2024-04-24 12:20:16 +0000

    working on test cases

--- src/python/agt/dqn_test.py
+++ src/python/agt/dqn_test.py
@@ -3,4 +3,11 @@ from random import randint
 
 from tf_agents.specs import array_spec
 
-from generi
+from generic_environment import GenericEnv
+from dqn_agent import DqnAgent
+
+#params
+num_episode = 2000  # @param
+board_size = 9
+
+#env
