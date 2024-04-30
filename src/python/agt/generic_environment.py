8bea46b40d29f05c8108d60cd3c55dd5363d0d0f
AnAnarchist <130145319+AnAnarchist@users.noreply.github.com>
2024-04-30 05:32:16 +0000

    Version bump to 2.1.1

--- src/python/agt/generic_environment.py
+++ src/python/agt/generic_environment.py
@@ -3,4 +3,8 @@ import numpy as np
 
 from tf_agents.environments import py_environment
 from tf_agents.specs import array_spec
-from tf_agents.trajectories i
+from tf_agents.trajectories import time_step as ts
+
+class GenericEnv(py_environment.PyEnvironment):
+
+  def __init__(self, actions, observations, initial
