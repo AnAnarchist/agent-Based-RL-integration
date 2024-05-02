8c695d8dc5e2544efc5105d62bc6052e81019ba8
AnAnarchist <130145319+AnAnarchist@users.noreply.github.com>
2024-05-02 12:43:55 +0000

    typo fixing

--- src/python/agt/generic_tf_agent.py
+++ src/python/agt/generic_tf_agent.py
@@ -6,4 +6,6 @@ import random
 import ast
 from abc import ABC, abstractmethod
 
-from tf_agents.environments
+from tf_agents.environments import tf_py_environment
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.trajectories 
