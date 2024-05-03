3fe575ce3f69cdd8d061e3ea1f26c7a2f3a28dc3
AnAnarchist <130145319+AnAnarchist@users.noreply.github.com>
2024-05-03 18:24:45 +0000

    generated

--- src/python/agt/generic_tf_agent.py
+++ src/python/agt/generic_tf_agent.py
@@ -11,4 +11,11 @@ from tf_agents.replay_buffers import tf_uniform_replay_buffer
 from tf_agents.trajectories import trajectory
 from tf_agents.utils import common
 from tf_agents.specs import array_spec
-from tf_agents.policies import ran
+from tf_agents.policies import random_tf_policy
+
+from generic_environment import GenericEnv
+
+tf.compat.v1.enable_v2_behavior()
+
+class GenericTfAgent(ABC):
+  def __init__(sel
