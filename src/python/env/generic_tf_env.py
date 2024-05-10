06d27831d1bfe552ad99a113d8ff8f8d3f83f66c
AnAnarchist <130145319+AnAnarchist@users.noreply.github.com>
2024-05-10 14:29:15 +0000

    fixed includes

--- src/python/env/generic_tf_env.py
+++ src/python/env/generic_tf_env.py
@@ -2,4 +2,8 @@ from tf_agents.environments import suite_gym
 
 class GenericTfEnv():
   def __init__(self, env_name, params={}):
-    #
+    #params
+    self.env_name = env_name
+    self.show_gui = params.get('show_gui', 'false') == 'true'
+
+    self.env = suite_gym.load(env
